5b353a124673aed45f9c849ce4ddc94081a85f3b
XiaoMing(Justin) <117292775@qq.com>
2015-02-08 01:31:09 +0800

    add box size

--- src/layout/Padding.ts
+++ src/layout/Padding.ts
@@ -2,10 +2,20 @@ module WOZLLA.layout {
 
     export class Padding {
 
+        get width():number {
+            return this.boxWidth - this.left - this.right
+        }
+
+        get height():number {
+            return this.boxHeight - this.top - this.bottom;
+        }
+
         constructor(public top:number,
                     public left:number,
                     public bottom:number,
-                    public right:number) {
+                    public right:number,
+                    public boxWidth:number=0,
+                    public boxHeight:number=0) {
 
         }
 
