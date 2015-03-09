2a4391b82c82f87b833f3c2917d79b261bf296a2
XiaoMing(Justin) <117292775@qq.com>
2015-03-09 11:13:59 +0800

    uglify js file

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ cd ${workpath}
 
 find src -name "*.ts" >> files.txt
 tsc -d @files.txt --out WOZLLA.js -t ES5 --sourceMap
+uglifyjs WOZLLA.js -o WOZLLA.min.js
 cp WOZLLA.js ./examples/
 rm files.txt
